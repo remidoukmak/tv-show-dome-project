eb67f9c515e89f276c2dbe05d3315e5485e11113
remidoukmak <73621087+remidoukmak@users.noreply.github.com>
2021-03-01 11:20:07 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const episodeType = document.createElement("kinds");
 const xp = document.getElementById("joyfulTime");
 xp.appendChild(episodeType);
 const prime = document.getElementById("review");
+const showItem = document.getElementById("show");
+
+// create element in html
+//declare a variable with that html element
+// populate it drop menu with shows
+// get value of selected show
 
 function setup() {
   makePageForEpisodes(allEpisodes);
@@ -28,21 +34,39 @@ ${item.season.toString().padStart(2, 0)}E${item.number.toString()}
   });
 }
 
-function searchForEpisodes(epList) {
+//creates search box and functionality
+function searchFunction(episodeList) {
   searchBox.addEventListener("input", (e) => {
-    // document.getElementById("root").innerHTML = "";
-    const searchEp = e.target.value.toLowerCase();
-    const filterEpList = epList.filter((episode) => {
+    e.preventDefault();
+    const searchString = e.target.value.toLowerCase();
+    let filteredEpisodes = episodeList.filter((epis) => {
       return (
-        episode.name.toLowerCase().includes(searchEp) ||
-        episode.summary.toLowerCase().includes(searchEp)
+        epis.name.toLowerCase().includes(searchString) ||
+        epis.summary.toLowerCase().includes(searchString)
       );
     });
     rootElem.innerHTML = "";
-    makePageForEpisodes(filterEpList);
+    pancakes.innerHTML = `${filteredEpisodes.length}/${episodeList.length} results found.`;
+    //the number of search results.
+    makePageForEpisodes(filteredEpisodes);
   });
 }
-searchForEpisodes(allEpisodes);
+
+// function searchForEpisodes(epList) {
+//   searchBox.addEventListener("input", (e) => {
+//     // document.getElementById("root").innerHTML = "";
+//     const searchEp = e.target.value.toLowerCase();
+//     const filterEpList = epList.filter((episode) => {
+//       return (
+//         episode.name.toLowerCase().includes(searchEp) ||
+//         episode.summary.toLowerCase().includes(searchEp)
+//       );
+//     });
+//     rootElem.innerHTML = "";
+//     makePageForEpisodes(filterEpList);
+//   });
+// }
+searchFunction(allEpisodes);
 
 // rootElem.innerHTML = "";
 
@@ -60,6 +84,7 @@ function dropDown(arr) {
   prime.insertAdjacentHTML("afterbegin", drops);
 }
 dropDown(allEpisodes);
+
 searchFunction(allEpisodes);
 function selectMenu(epList) {
   prime.addEventListener("change", (e) => {
@@ -82,71 +107,84 @@ function selectMenu(epList) {
 selectMenu(allEpisodes);
 window.onload = setup;
 
-// // eslint-disable-next-line no-undef
-// const allEpisodes = getAllEpisodes();
-// const searchBox = document.getElementById("livesearch");
-// const pancakes = document.createElement("span");
-// const rootElem = document.getElementById("root");
-// const clive = document.getElementById("pie");
-// const xp = document.getElementById("chocolatePancakes");
-// xp.appendChild(pancakes);
-// function setup() {
-//   makePageForEpisodes(allEpisodes);
-// }
-// function makePageForEpisodes(episodeList) {
-//   episodeList.forEach((item) => {
-//     rootElem.insertAdjacentHTML(
-//       "afterbegin",
-//       `<div class="episode">
-//       <h2>${item.name}</h2><div>
-//       <img src="${item.image.medium}" alt="">
-//       <br>
-//       ${item.summary}
-//       S${item.season.toString().padStart(2, 0)}
-//       E${item.number.toString().padStart(2, 0)}
-//       <br>
-//       <a href=${item.url}>Check the source</a>
-//     </div>`
-//     );
+// fetch("https://xkcd.now.sh/?comic=latest")
+//   .then(function (response) {
+//     let parsed = response.json();
+//     console.log(parsed);
+//     return parsed;
+//   })
+//   .then(function (response) {
+//     console.log(response);
+//     boxImg.src = response.img;
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     renderError(`Something wen wrong ${err.message}`);
 //   });
-// }
-// //creates search box and functionality
-// function searchFunction(episodeList) {
-//   searchBox.addEventListener("input", (e) => {
-//     e.preventDefault();
-//     const searchString = e.target.value.toLowerCase();
-//     let filteredEpisodes = episodeList.filter((epis) => {
-//       return (
-//         epis.name.toLowerCase().includes(searchString) ||
-//         epis.summary.toLowerCase().includes(searchString)
-//       );
-//     });
-//     rootElem.innerHTML = "";
-//     pancakes.innerHTML = `${filteredEpisodes.length}/${episodeList.length} results found.`; //the number of search results.
-//     makePageForEpisodes(filteredEpisodes);
-//   });
-// }
-// //select drop down bar
-// function dropDown(arr) {
-//   let drops = "";
-//   arr.forEach((item) => {
-//     drops += `<option value="${item.id}">${item.name}</option>`;
-//   });
-//   clive.innerHTML = "";
-//   clive.insertAdjacentHTML("afterbegin", drops);
-// }
-// dropDown(allEpisodes);
-// searchFunction(allEpisodes);
-// function selectMenu(episodeList) {
-//   clive.addEventListener("change", (e) => {
-//     e.preventDefault();
-//     const searchId = +e.target.value;
-//     let filteredList = episodeList.filter((epis) => {
-//       return epis.id === searchId;
-//     });
-//     rootElem.innerHTML = "";
-//     makePageForEpisodes(filteredList);
-//   });
-// }
-// selectMenu(allEpisodes);
-// window.onload = setup;
+
+getAllShows();
+
+const allShows = getAllShows();
+console.log(allShows);
+
+// fetch("https://api.tvmaze.com/shows/SHOW_ID/episodes")
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {});
+
+function listShows(shows) {
+  let tvShow = `<option value='1' >See all shows</option>`;
+  shows.sort(items => items.forEach((items) => {
+    tvShow += `<option value="${items.id}"> 
+      // S${item.season.toString().padStart(2, 0)}
+      // E${item.number.toString().padStart(2, 0)}
+      ${item.name}</option>`;
+  }));
+    
+  
+  show.innerHTML = "";
+  //replace prime with show
+
+  show.insertAdjacentHTML("afterbegin", tvShow);
+}
+
+
+function dropDown(shows) {
+  selection.addEventListener("change", (m))) => {
+    const searchId = +m, target.value;
+    let filteredlist = [];
+    if (searchId === 0) {
+      filteredList = shows;
+    }
+    else {
+      filteredList = shows.filter(popcorn) => {
+        return popcorn.id = searchId;
+      };
+    }
+    
+    show.innerHTML = "";
+    makePageForEpisodes(filteredList);
+    
+}
+
+
+function selectMenu(epList) {
+  //taking epList
+  prime.addEventListener("change", (e) => {
+    e.preventDefault();
+    const searchId = +e.target.value;
+    let filteredList = [];
+    console.log(searchId);
+    if (searchId === 1) {
+      filteredList = epList;
+    } else {
+      filteredList = epList.filter((episode) => {
+        return episode.id === searchId;
+      });
+    }
+    console.log(searchId);
+    rootElem.innerHTML = "";
+    makePageForEpisodes(filteredList);
+  });
+}
